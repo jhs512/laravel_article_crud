dc15d6e23a92c3a8e4307d1db6f6cdae478d0133
jhs512 <jangka512@gmail.com>
2022-02-02 18:02:09 +0900

    세팅

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <section class="section-1">
+        <div class="t-container t-mx-auto">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia, reprehenderit? Reprehenderit quasi expedita veritatis ad quos, velit harum incidunt fugit sapiente rerum cum saepe suscipit hic nisi eos ipsam eligendi.
+            <button class="btn btn-primary">
+                버튼입니다.
+            </button>
+        </div>
+    </section>
+</body>
+</html>
